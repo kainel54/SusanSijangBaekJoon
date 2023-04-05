100ff00b69850d28cc764e2ec974c7bc53630c56
kainel54 <kainel0504@gmail.com>
2023-04-05 16:30:27 +0900

    백준 27866번 풀이

--- SusanSijangBaekJoon/Program.cs
+++ SusanSijangBaekJoon/Program.cs
@@ -6,7 +6,10 @@ namespace SusanSijangBaekJoon
     {
         static void Main(string[] args)
         {
-            
+            string a = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a[b-1]);
         }
     }
 }
